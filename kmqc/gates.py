ca889efbd5bf06d0dd5fbe449a96cd3312cc7248
Rustam Sayfutdinov <rstm.sf@gmail.com>
2018-11-14 23:06:27 +0300

    Добавлена лицензия

--- kmqc/gates.py
+++ kmqc/gates.py
@@ -1,3 +1,25 @@
+# Copyright (C) 2018 Rustam Sayfutdinov, rstm.sf@gmail.com
+#
+# Permission is hereby granted, free of charge, to any person obtaining
+# a copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be
+# included in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+# NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS
+# BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+# ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import math
 
 from six import integer_types
@@ -19,6 +41,7 @@ class Rx(Gate):
     Rx(mu) = [[cos(mu / 2), -1j * sin(mu / 2)],
                [-1j * sin(mu / 2), cos(mu / 2)]]
     """
+
     def __init__(self, mu, qubit):
         params = {"mu": mu, }
         super().__init__("Rx", params, [_to_qubit(qubit), ])
@@ -29,6 +52,7 @@ class Ry(Gate):
     Ry(theta) = [[cos(theta / 2), -sin(theta / 2)],
                  [sin(theta / 2), cos(theta / 2)]]
     """
+
     def __init__(self, theta, qubit):
         params = {"theta": theta, }
         super().__init__("Ry", params, [_to_qubit(qubit), ])
@@ -39,6 +63,7 @@ class Rz(Gate):
     Rz(phi) = [[exp(-1j * phi / 2), 0]
                [0, exp(1j * phi / 2)]]
     """
+
     def __init__(self, phi, qubit):
         params = {"phi": phi, }
         super().__init__("Rz", params, [_to_qubit(qubit), ])
@@ -49,6 +74,7 @@ class U1(Gate):
     U1(mu) = [[1, 0],
                [0, exp(1j * mu)]]
     """
+
     def __init__(self, mu, qubit):
         params = {"mu": mu, }
         super().__init__("U1", params, [_to_qubit(qubit), ])
@@ -58,6 +84,7 @@ class U2(Gate):
     """
     U2(mu) = Rz(phi + pi / 2) Rx(pi / 2) Rz(mu - pi / 2)
     """
+
     def __init__(self, phi, mu, qubit):
         params = {"phi": phi, "mu": mu, }
         super().__init__("U2", params, [_to_qubit(qubit), ])
@@ -67,6 +94,7 @@ class U3(Gate):
     """
     U3(mu) = Rz(phi + 3 * pi) Rx(pi / 2) Rz(theta + pi) Rx(pi / 2) Rz(mu)
     """
+
     def __init__(self, theta, phi, mu, qubit):
         params = {"theta": theta, "phi": phi, "mu": mu, }
         super().__init__("U3", params, [_to_qubit(qubit), ])
@@ -77,6 +105,7 @@ class Z(U1):
     Z = [[1, 0],
          [0, -1]]
     """
+
     def __init__(self, qibit):
         super().__init__(math.pi, qibit)
 
@@ -86,6 +115,7 @@ class S_H(U1):
     S = [[1, 0],
          [0, -1j]]
     """
+
     def __init__(self, qibit):
         super().__init__(-math.pi / 2.0, qibit)
 
@@ -95,6 +125,7 @@ class X(U3):
     X = [[0, 1],
          [1, 0]]
     """
+
     def __init__(self, qibit):
         super().__init__(math.pi, 0.0, math.pi, qibit)
 
@@ -104,6 +135,7 @@ class Y(U3):
     Y = [[0, 0 - 1j],
          [0 + 1j, 0]]
     """
+
     def __init__(self, qibit):
         super().__init__(math.pi, math.pi / 2.0, math.pi / 2.0, qibit)
 
